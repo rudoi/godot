c324836cb9bb6c6c445a902834d1596a72dec7fc
Nintorch <92302738+Nintorch@users.noreply.github.com>
2026-01-02 20:48:41 +0500

    Custom joypad data packets, DualSense Adaptive Triggers

--- drivers/sdl/joypad_sdl.cpp
+++ drivers/sdl/joypad_sdl.cpp
@@ -301,6 +301,14 @@ void JoypadSDL::Joypad::set_joy_light(const Color &p_color) {
 	SDL_SetJoystickLED(get_sdl_joystick(), p_color.get_r8(), p_color.get_g8(), p_color.get_b8());
 }
 
+bool JoypadSDL::Joypad::has_joy_adaptive_triggers() const {
+	return SDL_GetGamepadTypeForID(sdl_instance_idx) == SDL_GAMEPAD_TYPE_PS5;
+}
+
+bool JoypadSDL::Joypad::send_joy_packet(const void *p_data, int p_size) {
+	return SDL_SendJoystickEffect(get_sdl_joystick(), p_data, p_size);
+}
+
 SDL_Joystick *JoypadSDL::Joypad::get_sdl_joystick() const {
 	return SDL_GetJoystickFromID(sdl_instance_idx);
 }
